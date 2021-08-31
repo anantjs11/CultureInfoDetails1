f75a743419724cf26550b3d0ec8d9eb34fccb35f
Anant Joshi <anajoshi2@publicisgroupe.net>
2021-08-31 15:45:51 +0530

    first commit

---
+++ CultureInfoDetails1/Controllers/HomeController.cs
@@ -0,0 +1,62 @@
+﻿using CultureInfoDetails1.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Globalization;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CultureInfoDetails1.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+
+        public IActionResult CultureDetails()
+        {
+            var cultures = new List<CultureSpecification>();
+            var allCultures = CultureInfo.GetCultures(CultureTypes.AllCultures & ~CultureTypes.NeutralCultures).ToList();
+            foreach (var culture in allCultures)
+            {
+                var cultureSpec = new CultureSpecification { 
+                    CultureCode= culture.Name,
+                    Name=culture.DisplayName,
+                    DecimalSeparator=culture.NumberFormat.CurrencyDecimalSeparator,
+                    GroupSeparator=culture.NumberFormat.CurrencyGroupSeparator,
+                    DisplayName=culture.DisplayName,
+                    DecimalSeparatorUnicode= culture.NumberFormat.CurrencyDecimalSeparator.Select(t => $"\\u{Convert.ToUInt16(t):X4} ").FirstOrDefault(),
+                    GroupSeparatorUnicode= culture.NumberFormat.CurrencyGroupSeparator.Select(t => $"\\u{Convert.ToUInt16(t):X4} ").FirstOrDefault()
+                };
+                cultures.Add(cultureSpec);
+            }
+            return View(cultures);
+
+        }
+    }
+
+
+}
---
+++ CultureInfoDetails1/Models/CultureSpecification.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CultureInfoDetails1.Models
+{
+    public class CultureSpecification
+    {
+        public string Name { get; set; }
+        public string GroupSeparator { get; set; }
+        public string CultureCode { get; set; }
+        public string DisplayName { get; set; }
+        public string DecimalSeparator  { get; set; }
+
+        public string GroupSeparatorUnicode { get; set; }
+
+        public string DecimalSeparatorUnicode { get; set; }
+    }
+}
